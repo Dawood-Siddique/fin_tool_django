cd07962ca9c9ba60f00e305620c49cacea71c88b
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-06-11 19:34:43 +0500

    added files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Created the project by
+- uv init .
+- uv add django
+- uv run django-admin startproject fin_django .
+
+
+# Adding tailwindcss
+https://django-tailwind.readthedocs.io/en/latest/installation.html
+
+- uv add install 'django-tailwind[reload]'
+- add 'tailwind' to INSTALLLED_APPS
+- uv run manage.py tailwind init
+- add theme to INSTALLED_APPS
+- TAILWIND_APP_NAME = 'theme'
+- uv run manage.py tailwind install
--- fin_django/settings.py
+++ fin_django/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party
+    'tailwind',
+    'theme',
+
+
+    #apps
+    'apps.userAuth',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
