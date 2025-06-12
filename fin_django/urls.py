5dcef1b16c9f5360a796f9754a9904ceb41b6b7a
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-06-12 15:44:27 +0500

    added home page working

--- fin_django/urls.py
+++ fin_django/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.userAuth.urls')),
-    path('__reload__/', include('django_browser_reload.urls'))
+    path('__reload__/', include('django_browser_reload.urls')),
+    path('', include('apps.home.urls')),
 ]
